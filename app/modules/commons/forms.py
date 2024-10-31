a827165c92fbd4d0722bc18624a61ffc5afd163e
Renato Marino Henz <renatomarinohenz@gmail.com>
2024-10-31 17:05:40 +0000

    refactor commons module

--- app/modules/commons/forms.py
+++ app/modules/commons/forms.py
@@ -1,23 +1,13 @@
-# -*- coding: utf-8 -*-
+"""Validation forms for the commons module."""
 
-# Import Form
 from wtforms import Form
-
-# Import JSON extension for forms
 import wtforms_json
-
-# Import Form elements such as TextField and BooleanField (optional)
-from wtforms import TextField, RadioField, IntegerField, FloatField  # BooleanField
-
-# Import Form validators
+from wtforms import TextField, IntegerField
 from wtforms.validators import InputRequired, Optional, Length
-from app.modules.utils import DateField
 
-# Initiating JSON for forms
 wtforms_json.init()
 
 
-# Define the create UF form (WTForms)
 class CreateUFForm(Form):
     code = TextField(
         "Code", [InputRequired(message="You must provide a code."), Length(max=128)]
@@ -27,13 +17,11 @@ class CreateUFForm(Form):
     )
 
 
-# Define the update UF form (WTForms)
 class UpdateUFForm(Form):
     code = TextField("Code", [Optional(), Length(max=128)])
     name = TextField("Name", [Optional(), Length(max=128)])
 
 
-# Define the create city form (WTForms)
 class CreateCityForm(Form):
     name = TextField(
         "Name", [InputRequired(message="You must provide a name."), Length(max=128)]
@@ -43,7 +31,6 @@ class CreateCityForm(Form):
     )
 
 
-# Define the update city form (WTForms)
 class UpdateCityForm(Form):
     name = TextField("Name", [Optional(), Length(max=128)])
     uf_id = IntegerField("UF ID", [Optional()])
